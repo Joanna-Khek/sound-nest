1c153fc64fa0ed825a6d602a6fe32cdcf5f7c9f8
Joanna <joannakhek@gmail.com>
2025-04-29 12:15:20 +0800

    feat: added logo to home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,13 +9,15 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="bg-white">
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="relative isolate px-5 lg:px-5">
+        <div className="mx-auto max-w-2xl">
+          <img
+            src="https://raw.githubusercontent.com/Joanna-Khek/sound-nest/refs/heads/main/frontend/public/sound_nest_logo_1.svg"
+            alt="Sound Nest Logo"
+            className="mx-auto block w-150 h-auto"
+          />
           <div className="text-center">
-            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              <img src="https://www.canva.com/design/DAGl-jSGoFg/jq-kgahqxdwCw7VfmjwwPA/view" />
-            </h1>
-            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+            <p className="text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
               Explore your music preferences and discover new sounds
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
